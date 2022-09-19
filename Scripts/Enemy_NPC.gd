9460059d100a63d23d64b342ee6b7a21bc0e44ba
yeet227 <jakethesnake2272004@gmail.com>
2022-09-20 09:48:23 +1000

    File Changes
    + Added Main Menu Color Change (Does not Work)
    + Further Changes Enemy Script

--- Scripts/Enemy_NPC.gd
+++ Scripts/Enemy_NPC.gd
@@ -1,6 +1,6 @@
 extends KinematicBody
 
-var Speed = 15.0
+var Speed = 6.0
 var point = Vector3(10, 10, 0)
 var Second_point = Vector3(52, 30, 0)
 
@@ -13,5 +13,9 @@ func _process(delta):
 	else:
 		direction = point - transform.origin
 	move_and_slide(direction)
+	print(transform)
 		
-		
+
+
+func _on_Area_body_entered(KinematicBody):
+	print(transform)
