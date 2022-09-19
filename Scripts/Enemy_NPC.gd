d694c27fc6caee0572d95e1cb2f193d737cdae30
Yeet227 <jakethesnake2272004@gmail.com>
2022-09-19 11:31:09 +1000

    Enemy Changes
    + Script Changes
    + Refactoring Enemy Scene

--- Scripts/Enemy_NPC.gd
+++ Scripts/Enemy_NPC.gd
@@ -1,16 +1,9 @@
-extends Node
+extends KinematicBody
 
+var Speed = 15.0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	
+
+func _on_Area_body_entered(body):
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
