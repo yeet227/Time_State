39bb2ce10602bd2df4e5152e30479968b4740545
yeet227 <jakethesnake2272004@gmail.com>
2022-09-20 10:35:27 +1000

    Wood Fire Asset
    + Added Wood Fire Asset with material
    + Added Particle System for Fire
    + Added Partial support Light Direction Movement

--- Scenes/Light_Movement.gd
+++ Scenes/Light_Movement.gd
@@ -1,7 +1,7 @@
-extends KinematicBody
+extends DirectionalLight
 
-var Speed = 6.0
-var point = Vector3(10, 10, 0)
+var Speed = 8.0
+var point = Vector3(10, 153.0, -128.8)
 
 func _process(delta):
 	var direction
@@ -11,13 +11,5 @@ func _process(delta):
 		direction = direction.normalized() * Speed
 	else:
 		direction = point - transform.origin
-	move_and_slide(direction)
 	#print(transform)
 		
-
-
-func _on_Area_body_entered(KinematicBody):
-	print("Hi!")
-	
-func _on_Area_body_exited(KinematicBody):
-	print("Good Bye!")
