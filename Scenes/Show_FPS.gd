b339f709375abc02cc2c691e99073c9fc88253b5
yeet227 <jakethesnake2272004@gmail.com>
2022-10-20 23:40:05 +1100

    Engine Config Changes and More
    + Added a frame delay for testing purposes
    + Commented Show_FPS.GD File for ease of interpretation

--- Scenes/Show_FPS.gd
+++ Scenes/Show_FPS.gd
@@ -1,6 +1,6 @@
 extends Node
 
 func _on_Show_FPS_pressed():
-	print(Engine.get_frames_per_second())
-	Global.ShowFPS = !Global.ShowFPS
-	print(Global.ShowFPS)
+	print(Engine.get_frames_per_second()) #This particular function retrives the frame rate data and outputs the data to the output logger
+	Global.ShowFPS = !Global.ShowFPS #Connecting to the Global Script regarding either False or True Statements to enable button to read the games frame rate from the Output Logger
+#	print(Global.ShowFPS) Debuggin Purposes to see the the Boolean Works
