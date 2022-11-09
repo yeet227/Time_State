664d500344b3b20ebbbe8a7f0b243f9c0e5c3d1a
yeet227 <jakethesnake2272004@gmail.com>
2022-11-09 17:09:08 +1100

    Additions and partial implemenations
    + Added Launch_Pad Mesh
    
    * Moved Parts of the map arround to give better workflow

--- Scenes/Show_FPS.gd
+++ Scenes/Show_FPS.gd
@@ -1,6 +1,6 @@
 extends Button
 
+
+
 func _on_Show_FPS_pressed():
-	print(Engine.get_frames_per_second()) #This particular function retrives the frame rate data and outputs the data to the output logger
-	Global.ShowFPS = !Global.ShowFPS #Connecting to the Global Script regarding either False or True Statements to enable button to read the games frame rate from the Output Logger
-	print(Global.ShowFPS) #Debugging Purposes to see the the Boolean Works
+	Button.text = str("Thanks For Playing!")
