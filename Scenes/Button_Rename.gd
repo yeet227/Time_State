694eeb312bad00ceb4a2c3d9f41decd0cc626b05
yeet227 <jakethesnake2272004@gmail.com>
2022-11-14 17:41:39 +1100

    Last Minute Code Commenting

--- Scenes/Button_Rename.gd
+++ Scenes/Button_Rename.gd
@@ -2,5 +2,5 @@ extends Button
 
 
 
-func _on_Show_FPS_pressed():
-	Button.text = str("Thanks For Playing!")
+func _on_Show_FPS_pressed(): #This function was mean't to Change the text on the button.
+	Button.text = str("Thanks For Playing!") #As referenced above the text was supposed to change after being pressed to say "THanks for Playing!"
